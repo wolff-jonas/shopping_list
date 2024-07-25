73636bccbdce37ea538986f06a876d2d25f08c90
Jonas Wolff <7514001+wolff-jonas@users.noreply.github.com>
2024-07-25 12:38:57 +0900

    Formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,13 +64,7 @@ const TABS: Tab[] = [
     {slug: "/presets", text: "Presets"}
 ]
 
-export default function RootLayout({
-                                       children,
-                                   }:
-                                       {
-                                           children: React.ReactNode;
-                                       }
-) {
+export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <head>
