cfda7e2db2b87717b5c4842de9886fe7dbbf4cdc
Jonas Wolff <7514001+wolff-jonas@users.noreply.github.com>
2024-09-16 13:25:31 +0900

    Added new icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,9 +75,15 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
             <meta name="mobile-web-app-capable" content="yes"/>
             <meta name="theme-color" content="#a9b1f1"/>
 
-            <link rel="icon" type="image/png" href="/icons/favicon.png"/>
             <link rel="manifest" href="/manifest.json"/>
-            <link rel="shortcut icon" href="/favicon.ico"/>
+            <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png"/>
+            <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5"/>
+            <link rel="shortcut icon" href="/icons/favicon.ico"/>
+            <meta name="msapplication-TileColor" content="#da532c"/>
+            <meta name="msapplication-config" content="/icons/browserconfig.xml"/>
+            <meta name="theme-color" content="#ffffff"/>
 
         </head>
         <body>
